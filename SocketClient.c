80b5e0a2a29470fd02d436b128d90b9fd5bc3cd4
ahmed-seghairoon <ahmmedmohammed912@gmail.com>
2023-12-30 03:59:22 +0300

    updated the client to keep reading input from the console and sending it to the server

--- SocketClient.c
+++ SocketClient.c
@@ -1,3 +1,4 @@
+#define  _GNU_SOURCE
 #include<Windows.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -9,46 +10,6 @@
 #define WIN32_LEAN_AND_MEAN
 
 #pragma comment(lib, "ws2_32.lib")
-
-void wslInit();
-int createTCPIpv4Socket();
-struct sockaddr_in* CreateIPv4Address(char* ip, int port);
-
-int main(){
-
-    wslInit();
-
-    int socketFD = createTCPIpv4Socket();
-
-    /*
-        we can use the socket file descriptor to connect to an idle socket
-        we have to specifiy the address of the service we used the sockaddr_in struct to represent ipv4 address
-        or we can use sockaddr_in6 to for ipv6
-    */
-    struct sockaddr_in* address = CreateIPv4Address("127.0.0.1", 2000);
-    int result = connect(socketFD, (SOCKADDR *) address, sizeof(*address));
-
-
-    if (result == 0)
-        printf("connection is successful\n");
-    else{
-        printf("connection failed\n");
-        exit(0);
-    }
-
-
-    char* messagee;
-    messagee = "GET \\ HTTP/1.1\r\nHOST:google.com\r\n\r\n";
-    send(socketFD, messagee, strlen(messagee), 0);
-
-    char buffer[1024];
-    recv(socketFD, buffer, 1024, 0);
-
-    printf("Response was %s\n", buffer);
-
-    closesocket(socketFD); //close the socket
-    WSACleanup(); // clean up the Windows Sockets library
-}
 
 
 // reusable function used to initialize the Windows Sockets library
@@ -91,6 +52,113 @@ struct sockaddr_in* CreateIPv4Address(char* ip, int port){
     address->sin_addr.s_addr = inet_addr(ip); // the ip address the inet_addr function converets the string to an unsigend int
     return address;
 }
+
+
+// getline function windows implementation
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL) {
+        return -1;
+    }
+    if (stream == NULL) {
+        return -1;
+    }
+    if (n == NULL) {
+        return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+    if (bufptr == NULL) {
+        bufptr = malloc(128);
+        if (bufptr == NULL) {
+            return -1;
+        }
+        size = 128;
+    }
+    p = bufptr;
+    while(c != EOF) {
+        if ((p - bufptr) > (size - 1)) {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL) {
+                return -1;
+            }
+        }
+        *p++ = c;
+        if (c == '\n') {
+            break;
+        }
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+
+int main(){
+
+    wslInit();
+
+    int socketFD = createTCPIpv4Socket();
+
+    /*
+        we can use the socket file descriptor to connect to an idle socket
+        we have to specifiy the address of the service we used the sockaddr_in struct to represent ipv4 address
+        or we can use sockaddr_in6 to for ipv6
+    */
+    struct sockaddr_in* address = CreateIPv4Address("127.0.0.1", 2000);
+    int result = connect(socketFD, (SOCKADDR *) address, sizeof(*address));
+
+
+    if (result == 0)
+        printf("connection is successful\n");
+    else{
+        printf("connection failed\n");
+        exit(0);
+    }
+
+
+    // reading a line from the terminal using stdin
+    // using line as the message buffer
+    char *line = NULL;
+    size_t lineSize = 0;
+    printf("Type a message (enter exit to exit the group chat):\n");
+
+    while (1)
+    {
+
+        //get line function returns the char count (the count of chracters read from the terminal)
+        SSIZE_T charCount = getline(&line, &lineSize, stdin);
+
+        // check if the message is not empty
+        //if the message is exit break out the loop
+        //else send the message
+        if (charCount > 0)
+        {
+            if (strcmp(line, "exit\n") == 0)
+                break;
+            SSIZE_T amountWasSent = send(socketFD, line, charCount, 0);
+        }
+        
+        
+    }
+
+    closesocket(socketFD); //close the socket
+    WSACleanup(); // clean up the Windows Sockets library
+}
+
 
 /*
     use command  
